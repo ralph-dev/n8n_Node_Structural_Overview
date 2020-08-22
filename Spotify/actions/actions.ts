f3462fc03c23bf16e40227112b66cff0063f5605
ralph-dev <ralphpal@hotmail.com>
2020-08-21 17:00:08 -0700

    Left a comment

--- Spotify/actions/actions.ts
+++ Spotify/actions/actions.ts
@@ -2,6 +2,7 @@ import {HTTPMethod} from "../../n8nNodeSupport/n8nExternalUse/types";
 import {IAPI_ACTIONS} from "../../n8nNodeSupport/n8nInternalUse/helpers";
 import {getRandomPlaylist, getPlaylist, getSong} from "./operations/index";
 
+
 /* Owned by Individual API Node */
 const spotifyActions: IAPI_ACTIONS = {
     resources: {
@@ -20,3 +21,6 @@ const spotifyActions: IAPI_ACTIONS = {
         },
     },
 };
+
+// To execute, conduct a DownWard Crawl from Top to Bottom to get
+// the request being built as the crawl continues.
